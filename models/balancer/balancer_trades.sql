11b7f252926a3b978bed563eac1f1a904322eef1
Jacek Trocinski <jacek@hedgehog.app>
2022-12-02 13:16:23 +0100

    Clean-up the code by using a JINJA `for` loop.

--- models/balancer/balancer_trades.sql
+++ models/balancer/balancer_trades.sql
@@ -7,114 +7,45 @@
         )
 }}
 
+{% set balancer_models = [
+ 'balancer_arbitrum_trades'
+,'balancer_ethereum_trades'
+,'balancer_optimism_trades'
+,'balancer_polygon_trades'
+] %}
+
+
 SELECT *
 FROM
 (
+        {% for dex_model in balancer_models %}
         SELECT
-            blockchain,
-            project,
-            version,
-            block_date,
-            block_time,
-            token_bought_symbol,
-            token_sold_symbol,
-            token_pair,
-            token_bought_amount,
-            token_sold_amount,
-            token_bought_amount_raw,
-            token_sold_amount_raw,
-            amount_usd,
-            token_bought_address,
-            token_sold_address,
-            taker,
-            maker,
-            project_contract_address,
-            tx_hash,
-            tx_from,
-            tx_to,
-            trace_address,
-            evt_index
-        FROM
-            {{ ref ('balancer_arbitrum_trades') }}
+                blockchain
+                ,project
+                ,version
+                ,block_date
+                ,block_time
+                ,token_bought_symbol
+                ,token_sold_symbol
+                ,token_pair
+                ,token_bought_amount
+                ,token_sold_amount
+                ,token_bought_amount_raw
+                ,token_sold_amount_raw
+                ,amount_usd
+                ,token_bought_address
+                ,token_sold_address
+                ,taker
+                ,maker
+                ,project_contract_address
+                ,tx_hash
+                ,tx_from
+                ,tx_to
+                ,trace_address
+                ,evt_index
+        FROM {{ ref(dex_model) }}
+        {% if not loop.last %}
         UNION ALL
-        SELECT
-            blockchain,
-            project,
-            version,
-            block_date,
-            block_time,
-            token_bought_symbol,
-            token_sold_symbol,
-            token_pair,
-            token_bought_amount,
-            token_sold_amount,
-            token_bought_amount_raw,
-            token_sold_amount_raw,
-            amount_usd,
-            token_bought_address,
-            token_sold_address,
-            taker,
-            maker,
-            project_contract_address,
-            tx_hash,
-            tx_from,
-            tx_to,
-            trace_address,
-            evt_index
-        FROM
-            {{ ref ('balancer_ethereum_trades') }}
-        UNION ALL
-        SELECT
-            blockchain,
-            project,
-            version,
-            block_date,
-            block_time,
-            token_bought_symbol,
-            token_sold_symbol,
-            token_pair,
-            token_bought_amount,
-            token_sold_amount,
-            token_bought_amount_raw,
-            token_sold_amount_raw,
-            amount_usd,
-            token_bought_address,
-            token_sold_address,
-            taker,
-            maker,
-            project_contract_address,
-            tx_hash,
-            tx_from,
-            tx_to,
-            trace_address,
-            evt_index
-        FROM
-            {{ ref ('balancer_optimism_trades') }}
-        UNION ALL
-        SELECT
-            blockchain,
-            project,
-            version,
-            block_date,
-            block_time,
-            token_bought_symbol,
-            token_sold_symbol,
-            token_pair,
-            token_bought_amount,
-            token_sold_amount,
-            token_bought_amount_raw,
-            token_sold_amount_raw,
-            amount_usd,
-            token_bought_address,
-            token_sold_address,
-            taker,
-            maker,
-            project_contract_address,
-            tx_hash,
-            tx_from,
-            tx_to,
-            trace_address,
-            evt_index
-        FROM
-            {{ ref ('balancer_polygon_trades') }}
+        {% endif %}
+        {% endfor %}
 )
