8295defda6a48848dfa9709ae6f617463bbdb2fe
Jacek Trocinski <jacek@hedgehog.app>
2023-01-23 11:48:25 +0100

    Add `bpt_prices`.

--- models/balancer/polygon/balancer_v2_polygon_trades.sql
+++ models/balancer/polygon/balancer_v2_polygon_trades.sql
@@ -32,6 +32,7 @@ WITH swap_fees AS (
 ),
 dexs AS (
     SELECT
+        swap.evt_block_number,
         swap.evt_block_time AS block_time,
         '' AS taker,
         '' AS maker,
@@ -47,11 +48,11 @@ dexs AS (
         swap.evt_index
     FROM
         swap_fees
-        INNER JOIN {{ source ('balancer_v2_polygon', 'Vault_evt_Swap') }} swap
+        INNER JOIN {{ source ('balancer_v2_ethereum', 'Vault_evt_Swap') }} swap
             ON swap.evt_block_number = swap_fees.evt_block_number
             AND swap.evt_tx_hash = swap_fees.evt_tx_hash
             AND swap.evt_index = swap_fees.evt_index
-        LEFT JOIN {{ ref('balancer_v2_polygon_pools_fees') }}
+        LEFT JOIN {{ ref('balancer_v2_ethereum_pools_fees') }}
             ON pools_fees.contract_address = swap_fees.contract_address
             AND pools_fees.block_time = swap_fees.max_fee_evt_block_time
     WHERE
@@ -61,6 +62,50 @@ dexs AS (
         {% if is_incremental() %}
         AND swap.evt_block_time >= DATE_TRUNC("day", NOW() - interval '1 week')
         {% endif %}
+),
+bpa AS (
+    SELECT
+        dexs.evt_block_number,
+        dexs.tx_hash,
+        dexs.evt_index,
+        bpt_prices.contract_address,
+        dexs.block_time,
+        MAX(bpt_prices.hour) AS bpa_max_block_time
+    FROM
+        dexs
+        LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpt_prices
+            ON bpt_prices.contract_address = dexs.token_bought_address
+            AND bpt_prices.hour <= dexs.block_time
+            -- Incremental logic.
+            {% if not is_incremental () %}
+            AND bpt_prices.hour >= '{{ project_start_date }}'
+            {% endif %}
+            {% if is_incremental () %}
+            AND bpt_prices.hour >= DATE_TRUNC("day", NOW() - interval '1 week')
+            {% endif %}
+    GROUP BY 1, 2, 3, 4, 5
+),
+bpb AS (
+    SELECT
+        dexs.evt_block_number,
+        dexs.tx_hash,
+        dexs.evt_index,
+        bpt_prices.contract_address,
+        dexs.block_time,
+        MAX(bpt_prices.hour) AS bpb_max_block_time
+    FROM
+        dexs
+        LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpt_prices
+            ON bpt_prices.contract_address = dexs.token_sold_address
+            AND bpt_prices.hour <= dexs.block_time
+            -- Incremental logic.
+            {% if not is_incremental () %}
+            AND bpt_prices.hour >= '{{ project_start_date }}'
+            {% endif %}
+            {% if is_incremental () %}
+            AND bpt_prices.hour >= DATE_TRUNC("day", NOW() - interval '1 week')
+            {% endif %}
+    GROUP BY 1, 2, 3, 4, 5
 )
 SELECT
     'polygon' AS blockchain,
@@ -82,7 +127,9 @@ SELECT
     COALESCE(
         dexs.amount_usd,
         dexs.token_bought_amount_raw / POWER(10, p_bought.decimals) * p_bought.price,
-        dexs.token_sold_amount_raw / POWER(10, p_sold.decimals) * p_sold.price
+        dexs.token_sold_amount_raw / POWER(10, p_sold.decimals) * p_sold.price,
+        dexs.token_bought_amount_raw / POWER(10, erc20a.decimals) * bpa_bpt_prices.median_price,
+        dexs.token_sold_amount_raw / POWER(10, erc20b.decimals)  * bpb_bpt_prices.median_price
     ) AS amount_usd,
     dexs.token_bought_address,
     dexs.token_sold_address,
@@ -134,5 +181,33 @@ FROM
         {% endif %}
         {% if is_incremental () %}
         AND p_sold.minute >= DATE_TRUNC("day", NOW() - interval '1 week')
+        {% endif %}
+    INNER JOIN bpa
+        ON bpa.evt_block_number = dexs.evt_block_number
+        AND bpa.tx_hash = dexs.tx_hash
+        AND bpa.evt_index = dexs.evt_index
+    LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpa_bpt_prices
+        ON bpa_bpt_prices.contract_address = bpa.contract_address
+        AND bpa_bpt_prices.hour = bpa.bpa_max_block_time
+        -- Incremental logic.
+        {% if not is_incremental () %}
+        AND bpa_bpt_prices.hour >= '{{ project_start_date }}'
+        {% endif %}
+        {% if is_incremental () %}
+        AND bpa_bpt_prices.hour >= DATE_TRUNC("day", NOW() - interval '1 week')
+        {% endif %}
+    INNER JOIN bpb
+        ON bpb.evt_block_number = dexs.evt_block_number
+        AND bpb.tx_hash = dexs.tx_hash
+        AND bpb.evt_index = dexs.evt_index
+    LEFT JOIN {{ ref('balancer_v2_polygon_bpt_prices') }} bpb_bpt_prices
+        ON bpb_bpt_prices.contract_address = bpb.contract_address
+        AND bpb_bpt_prices.hour = bpb.bpb_max_block_time
+        -- Incremental logic.
+        {% if not is_incremental () %}
+        AND bpb_bpt_prices.hour >= '{{ project_start_date }}'
+        {% endif %}
+        {% if is_incremental () %}
+        AND bpb_bpt_prices.hour >= DATE_TRUNC("day", NOW() - interval '1 week')
         {% endif %}
 ;
